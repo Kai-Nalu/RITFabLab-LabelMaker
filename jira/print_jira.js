f1936d31aa6ce37192f7a8038ee901bea722dd6b
Kai-Nalu <kainietogonzalez@gmail.com>
2023-02-08 17:44:59 -0500

    fixed package.json

--- jira/print_jira.js
+++ jira/print_jira.js
@@ -3,10 +3,10 @@ exports.print_jira = function(args) {
         const jiraHandler = require("./jiraHandler");
         jiraHandler.jiraHandler(args).then(function(result) {
             const exec = require('child_process').exec;
-            const generate_label_png = require("./generate_label_png");
+            const generate_label_png = require("../generate_label_png");
             
             generate_label_png.generate_label_png(result).then(function(){
-                exec('lp -d DYMO_LabelWriter_450 ./output/label.png');
+                exec('lp -d DYMO_LabelWriter_450 ../output/label.png');
                 resolve();
             });
         });
