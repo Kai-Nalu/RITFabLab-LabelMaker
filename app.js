c145856474910cfc6c12a56ed49bbb52055ecfb6
Kai-Nalu <kainietogonzalez@gmail.com>
2024-01-12 14:05:32 -0500

    Added update api key function

--- app.js
+++ app.js
@@ -24,6 +24,23 @@ app.get('/status', (req, res) => {
   console.debug(`Test connection from ${req.socket.remoteAddress}`);
 });
 
+const config_handler = require("./config_handler");
+
+app.get('/update-api-key', (req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin": req.headers.origin
+  });
+  config_handler.updateApiKey(req.query.key, req.query.type).then((success) => {
+    if (success) {
+      res.sendStatus(200);
+      console.debug(`Updated ${req.query.type} API key from ${req.socket.remoteAddress}`);
+    } else {
+      res.sendStatus(500);
+      console.error(`Failed to update ${req.query.type} API key from ${req.socket.remoteAddress}`);
+    }
+  });
+});
+
 const print_deskpro = require("./deskpro/print_deskpro");
 const print_jira = require("./jira/print_jira");
 const print_handler = {"deskpro": print_deskpro, "jira": print_jira};
---
+++ config_handler.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const deskpro_key_path = "./deskpro/deskproAPIToken.txt";
+
+function updateApiKey(key, type) {
+	return new Promise (resolve => {
+		if (type = "deskpro") {
+			fs.writeFile(deskpro_key_path, key, err => {
+				if (err) {
+					console.error(err);
+					resolve(false);
+				} else {
+					resolve(true);
+				}
+			});
+		}
+	});
+}
+
+module.exports = { updateApiKey };
